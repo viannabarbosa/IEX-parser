360711656cb81786e1e2a5ef0abbc886cb94165f
Rodrigo Vianna <vianna.barbosa@gmail.com>
2022-09-04 21:29:50 -0400

    installing winpcap

--- IEXParser.cpp
+++ IEXParser.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+// tutorial: https://www.rhyous.com/2011/11/12/how-to-compile-winpcap-with-visual-studio-2010/
+// small error on Configuration 4 - should be wpcap.lib, not winpcap.lib
+// note: we are using x64 so path is Lib\x64 https://stackoverflow.com/questions/46890411/how-to-read-a-pcap-file-from-wireshark-with-visual-c
+#include <pcap.h>
 
 int main()
 {
